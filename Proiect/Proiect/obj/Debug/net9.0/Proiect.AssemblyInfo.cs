9753fd3d9069d347f2f5b6a5aff54da79d5a9ece
Ionela Secman <ionellaccl@gmail.com>
2025-11-14 14:22:58 +0200

    eu sper ca ne indreptam spre ok

--- Proiect/Proiect/obj/Debug/net9.0/Proiect.AssemblyInfo.cs
+++ Proiect/Proiect/obj/Debug/net9.0/Proiect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60adec397c32efadb3e201b45ad24847700c9953")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6445cd9b99ed59bf3ed3a82bab07d613fd9de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
